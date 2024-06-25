d5a795f4bc494f9ce11965c918cc29672b7e22c9
saurabhwakchaure <saurabhwakchaure1412@gmail.com>
2024-06-26 00:12:05 +0530

    fifth commit content library

---
+++ app/Controllers/Git4Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Git4Controller extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
